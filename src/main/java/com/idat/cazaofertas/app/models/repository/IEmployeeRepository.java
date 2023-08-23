546349a7914d602aa3d3b6a4bf355c191016d84f
unknown <A18101119@idat.edu.pe>
2023-08-23 09:48:04 -0500

    final

--- src/main/java/com/idat/cazaofertas/app/models/repository/IEmployeeRepository.java
+++ src/main/java/com/idat/cazaofertas/app/models/repository/IEmployeeRepository.java
@@ -1,11 +1,10 @@
 package com.idat.cazaofertas.app.models.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.idat.cazaofertas.app.models.entity.Employee;
 
-@Repository
+
 public interface IEmployeeRepository extends JpaRepository<Employee, Long>{
 
 }
